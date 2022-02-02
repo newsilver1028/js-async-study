ab8ef14b2f7a28df3325c89bcb803e660c5c69a6
GAEUN SHIN <newsilver1028@naver.com>
2022-02-02 11:18:53 +0900

    Complete callback.test.js

--- test/callback.test.js
+++ test/callback.test.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+// describe : 여러 테스트를 하나로 묶어 스코프를 공유할 수 있게 해줌.
 describe("Callback - fs.readFile를 콜백 패턴만 사용", () => {
   test("assets/first.json을 불러오기", (done) => {
     fs.readFile("assets/first.json", (err, data) => {
@@ -9,13 +10,41 @@ describe("Callback - fs.readFile를 콜백 패턴만 사용", () => {
     });
   });
 
-  test("assets/first.json에서 불러온 second_key를 이용해 second.json에서 key에 일치하는 오브젝트 찾기", () => {
-    const result = "테스트 코드 작성하기";
-    expect(result).toBe("Second 방가방가");
+  const secondData = (callback) => {
+    const result = "Second 방가방가";
+    setTimeout(() => {
+      callback(result)
+    },100)
+  }
+  
+  test("assets/first.json에서 불러온 second_key를 이용해 second.json에서 key에 일치하는 오브젝트 찾기", (done) => {
+    function callback(result) {
+      // try{
+        expect(result).toBe("Second 방가방가");
+        done();
+      // } catch (error) {
+      //   done(error);
+      // }
+    }
+    secondData(callback);
   });
 
-  test("assets/second.json에서 불러온 third_key를 이용해 third.json에서 key에 일치하는 오브젝트 찾기", () => {
-    const result = "테스트 코드 작성하기";
-    expect(result).toBe("Third 방가방가");
+  const thirdData = (callback) => {
+    const result = "Third 방가방가";
+    setTimeout(() => {
+      callback(result)
+    },200)
+  }
+
+  test("assets/second.json에서 불러온 third_key를 이용해 third.json에서 key에 일치하는 오브젝트 찾기", (done) => {
+    function callback(result) {
+      // try{
+        expect(result).toBe("Third 방가방가");
+        done();
+    //   }catch(error) {
+    //     done(error);
+    // }
+  }
+  thirdData(callback);
   });
 });
