34d56addba10fc7895f215d451307c7a7c33d94f
GAEUN SHIN <newsilver1028@naver.com>
2022-02-02 14:53:18 +0900

    Complete fs.readFile을 이용해 Promise를 반환하는 readFile 함수 만들기

--- src/readFile.js
+++ src/readFile.js
@@ -1,6 +1,11 @@
-export const readFile = (filePath) => {
-  // return Promise
-  // TODO
-  throw Error("fs.readFile을 이용해 Promise화 시킨 readFile을 구현하여 보세요");
-  // https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Promise
-};
+const fs = require("fs");
+
+const readFile = (filePath) => {
+  return new Promise((resolve,reject) => {
+    fs.readFile(filePath, (err,data) => {
+      resolve(data);
+    })
+  });
+}
+
+module.exports = {readFile}
