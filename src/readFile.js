3ef52fe3835e8adc27ea3de15c74824493dde6b9
GAEUN SHIN <newsilver1028@naver.com>
2022-02-11 15:15:40 +0900

    Fix typo

--- src/readFile.js
+++ src/readFile.js
@@ -2,10 +2,14 @@ const fs = require("fs");
 
 const readFile = (filePath) => {
   return new Promise((resolve,reject) => {
-    fs.readFile(filePath, (err,data) => {
-      resolve(data);
+    fs.readFile(filePath, (err, data) => {
+      if(err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
     })
   });
 }
 
-module.exports = {readFile}
+module.exports = {readFile};
