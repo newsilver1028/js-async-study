1f2b27f5c68c030510a10372314045dd74d2d96f
GAEUN SHIN <newsilver1028@naver.com>
2022-06-18 03:25:08 +0900

    refactor: promise and async/await

--- test/promise.test.js
+++ test/promise.test.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
 const { readFile } = require("../src/readFile");
 
 describe("Promise - fs.readFile를 프로미스 패턴만 사용", () => {
@@ -7,32 +6,32 @@ describe("Promise - fs.readFile를 프로미스 패턴만 사용", () => {
     expect(promise instanceof Promise).toBe(true);
     done();
   });
-  
+
   let secondKey = 0;
   let thirdKey = 0;
-  test("assets/first.json을 불러오기", (done) => {  
+
+  test("assets/first.json을 불러오기", (done) => {
     readFile("assets/first.json").then((data) => {
       const first = JSON.parse(data);
       secondKey = first.second_key;
+
       expect(first.hi).toBe("방가방가");
       done();
-    })
-  });
-  test("assets/first.json에서 불러온 second_key를 이용해 second.json에서 key에 일치하는 오브젝트 찾기", (done) => {
-    fs.readFile("assets/second.json", (err, data) => {
+
+      return readFile("assets/second.json");
+    }).then((data) => {
       const second = JSON.parse(data);
-      const result = second.filter((el) => el["key"] === secondKey);
-      thirdKey = result[0].third_key;
-      expect(result[0].hi).toBe("Second 방가방가");
+      const secondResult = second.find((item) => item.key === secondKey);
+      thirdKey = secondResult.third_key;
+      expect(secondResult.hi).toBe("Second 방가방가");
       done();
-    });
-  })
-  test("assets/second.json에서 불러온 third_key를 이용해 third.json에서 key에 일치하는 오브젝트 찾기", (done) => {
-    fs.readFile("assets/third.json",(err,data) => {
+
+      return readFile("assets/third.json");
+    }).then((data) => {
       const third = JSON.parse(data);
-      const result = third.filter((el) => el["key"] === thirdKey);
-      expect(result[0].hi).toBe("Third 방가방가");
+      const thirdResult = third.find((item) => item.key === thirdKey);
+      expect(thirdResult.hi).toBe("Third 방가방가");
       done();
-    });
-  });
+    })
+  })
 });
